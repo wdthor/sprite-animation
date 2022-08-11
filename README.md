4aae33125db3ee6a661b8cb49a12da769eefd9ce
wdthor <wdthor@proton.me>
2022-08-11 12:58:00 +0200

    Navigate sprite sheet * Fix css link * Add Readme * Move sprite image and styles in assets folder * Display single sprite in canvas

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sprites image specs
+- width: 6876px and 12 columns
+- 6876/12 = 573px ~575px with the margin
+- height: 5230px and 10 columns
+- 5230/10 = 523px
--- src/script.ts
+++ src/script.ts
@@ -3,20 +3,41 @@ const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
 
 // By default the canvas is set to 300X500px
 const CANVAS_WIDTH: number = (canvas.width = 600);
-const CANVAS_Height: number = (canvas.height = 600);
+const CANVAS_HEIGHT: number = (canvas.height = 600);
 
+/**
+ * Image width is 6876px and 12 columns
+ * Sprite width 6876/12 = 573px ~575px with the margin
+ * Image height is 5230px and 10 columns
+ * Sprite height 5230/10 = 523px
+ */
 const playerImage = new Image();
-playerImage.src = './shadow_dog.png';
-let x = 0;
+playerImage.src = './src/assets/images/shadow_dog.png';
+const SPRITE_WIDTH = 575;
+const SPRITE_HEIGHT = 523;
+
+let frameX = 1;
+let frameY = 1;
 
 /**
  * When the animation function is called
  * The canvas is cleared (reset)
  */
 const animate = () => {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_Height);
-  ctx.fillRect(x, 50, 100, 100);
-  x++;
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  // ctx.fillRect(50, 50, 100, 100);
+  // ctx.drawImage(playerImage, sx, sy, sw, sh, dx, dy, dw, dh);
+  ctx.drawImage(
+    playerImage,
+    frameX * SPRITE_WIDTH,
+    frameY * SPRITE_HEIGHT,
+    SPRITE_WIDTH,
+    SPRITE_HEIGHT,
+    0,
+    0,
+    CANVAS_WIDTH,
+    CANVAS_HEIGHT
+  );
   requestAnimationFrame(animate);
 };
 
